011f3bd9c791ffd5963b28a406919d1e2b274de3
Jannis Bautsch <jannis.bautsch@softgarden.de>
2022-06-07 22:33:43 +0200

    remove react build

--- index.js
+++ index.js
@@ -49,13 +49,6 @@ app.post("/api/newCourt", async (req, res) => {
   res.sendStatus(200);
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const port = process.env.PORT || 3080;
 
 app.listen(port, () => {
